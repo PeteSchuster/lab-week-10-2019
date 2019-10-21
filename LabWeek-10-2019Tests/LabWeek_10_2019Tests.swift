b09bf392f38c011f4d1ba2b5a16f44de279bfebb
Pete Schuster <peter_schuster@cable.comcast.com>
2019-10-21 13:40:00 -0400

    Initial Commit

---
+++ LabWeek-10-2019Tests/LabWeek_10_2019Tests.swift
@@ -0,0 +1,34 @@
+//
+//  LabWeek_10_2019Tests.swift
+//  LabWeek-10-2019Tests
+//
+//  Created by Pete Schuster on 10/21/19.
+//  Copyright © 2019 Pete Schuster. All rights reserved.
+//
+
+import XCTest
+@testable import LabWeek_10_2019
+
+class LabWeek_10_2019Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
