dd690f9ddaf3f445929b5ab6c21666ecd7565951
Pete Schuster <pas5027@gmail.com>
2019-10-22 09:56:56 -0400

    try to convert

--- LabWeek-10-2019/components/ARViewContainer.swift
+++ LabWeek-10-2019/components/ARViewContainer.swift
@@ -10,35 +10,86 @@ import SwiftUI
 import RealityKit
 import ARKit
 
-struct ARViewContainer: View {
-    var body: some View {
-        ARViewComponent()
+struct ARViewContainer: UIViewController, ARSCNViewDelegate {
+    var foes = [String: Foe]()
+
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let imageAnchor = anchor as? ARImageAnchor else { return nil }
+        guard let name = imageAnchor.referenceImage.name else { return nil }
+        guard let foe = foes[name] else { return nil }
+
+        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+        plane.firstMaterial?.diffuse.contents = UIColor.clear
+
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi / 2
+
+        let node = SCNNode()
+        node.addChildNode(planeNode)
+
+        let spacing: Float = 0.005
+
+        let titleNode = textNode(foe.name, font: UIFont.boldSystemFont(ofSize: 10))
+        titleNode.pivotOnTopLeft()
+
+        titleNode.position.x += Float(plane.width / 2) + spacing
+        titleNode.position.y += Float(plane.height / 2)
+
+        planeNode.addChildNode(titleNode)
+
+        return node
+    }
+
+    func loadData() {
+        guard let url = Bundle.main.url(forResource: "foes", withExtension: "json") else {
+            fatalError("Unable to find JSON in bundle")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Unable to load JSON")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let loadedFoes = try? decoder.decode([String: Foe].self, from: data) else {
+            fatalError("Unable to parse JSON.")
+        }
+
+        foes = loadedFoes
+    }
+
+    func textNode(_ str: String, font: UIFont, maxWidth: Int? = nil) -> SCNNode {
+        let text = SCNText(string: str, extrusionDepth: 0)
+
+        text.flatness = 0.1
+        text.font = font
+
+        if let maxWidth = maxWidth {
+            text.containerFrame = CGRect(origin: .zero, size: CGSize(width: maxWidth, height: 500))
+            text.isWrapped = true
+        }
+
+        let textNode = SCNNode(geometry: text)
+        textNode.scale = SCNVector3(0.002, 0.002, 0.002)
+
+        return textNode
     }
 }
 
-struct ARViewComponent: UIViewRepresentable {
-
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-        
-        // Create a session configuration
-        let configuration = ARImageTrackingConfiguration()
-
-        guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "foes", bundle: nil) else {
-            fatalError("Couldn't load tracking images")
-        }
-
-        configuration.trackingImages = trackingImages
-
-        // Run the view's session
-        arView.session.run(configuration)
-    
-        return arView
+extension SCNNode {
+    var height: Float {
+        return (boundingBox.max.y - boundingBox.min.y) * scale.y
     }
-    
-    func updateUIView(_ uiView: ARView, context: Context) {}
-    
+
+    func pivotOnTopLeft() {
+        let (min, max) = boundingBox
+        pivot = SCNMatrix4MakeTranslation(min.x, max.y, 0)
+    }
+
+    func pivotOnTopCenter() {
+        let (_, max) = boundingBox
+        pivot = SCNMatrix4MakeTranslation(0, max.y, 0)
+    }
 }
 
 struct ARViewContainer_Previews: PreviewProvider {
